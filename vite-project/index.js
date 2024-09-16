ef561b5a8f4ad9809e03aee9c6c88f2a4eb3bc26
Sasha Lapina <lapinasasha@gmail.com>
2024-09-16 23:38:53 +0300

    Change color selection from random algorithm to switch color group

--- vite-project/index.js
+++ vite-project/index.js
@@ -1,8 +1,13 @@
-import { setMainColor } from './js_modules/setMainColor.js'
+// import { setMainColor } from './js_modules/setMainColor.js'
 import { addItemToCart } from './js_modules/addItemToCart.js'
+import { setMainColorBtn } from './js_modules/setMainColor.js'
 
 const byButton = document.getElementById('sell-btn')
 const orderCart = document.getElementById('order-cart')
+const colorSwitchBtn1 = document.getElementById('color-btn-1')
+const colorSwitchBtn2 = document.getElementById('color-btn-2')
+const colorSwitchBtn3 = document.getElementById('color-btn-3')
 
-setMainColor(document)
+// setMainColor(document)
+setMainColorBtn(document, colorSwitchBtn1, colorSwitchBtn2, colorSwitchBtn3)
 addItemToCart(document, byButton, orderCart)
--- vite-project/js_modules/setMainColor.js
+++ vite-project/js_modules/setMainColor.js
@@ -16,3 +16,63 @@ export function setMainColor(document) {
       break
   }
 }
+
+export function setMainColorBtn(document, button1, button2, button3) {
+  const root = document.documentElement
+
+  const bgColor1Var = '#3D8168'
+  const bgColor1HoverVar = '#1A4032'
+  const bgColor2Var = '#6C7289'
+  const bgColor2HoverVar = '#4C5463'
+  const bgColor3Var = '#1C232B'
+  const bgColor3HoverVar = '#12181F'
+
+  document.addEventListener('DOMContentLoaded', () => {
+    setActive(button1, bgColor1Var)
+  })
+
+  button1.addEventListener('click', (event) => {
+    event.preventDefault()
+    setActive(button1, bgColor1Var)
+    clearActive(button2)
+    clearActive(button3)
+    changeColor(root, bgColor1Var, bgColor1HoverVar)
+  })
+
+  button2.addEventListener('click', (event) => {
+    event.preventDefault()
+    setActive(button2, bgColor2Var)
+    clearActive(button1)
+    clearActive(button3)
+    changeColor(root, bgColor2Var, bgColor2HoverVar)
+  })
+
+  button3.addEventListener('click', (event) => {
+    event.preventDefault()
+    setActive(button3, bgColor3Var)
+    clearActive(button1)
+    clearActive(button2)
+    changeColor(root, bgColor3Var, bgColor3HoverVar)
+  })
+}
+
+function setActive(button, bgcolor) {
+  button.style.width = '34px'
+  button.style.height = '34px'
+  button.style.borderRadius = '50%'
+  button.style.boxShadow = `0 0 0 3px ${bgcolor}`
+  button.style.border = '2px solid var(--cream-color)'
+}
+
+function clearActive(button) {
+  button.style.width = '36px'
+  button.style.height = '36px'
+  button.style.borderRadius = '50%'
+  button.style.boxShadow = 'none'
+  button.style.border = 'none'
+}
+
+function changeColor(root, color, hoverColor) {
+  root.style.setProperty('--main-active-color', color)
+  root.style.setProperty('--hover-main-active-color', hoverColor)
+}
